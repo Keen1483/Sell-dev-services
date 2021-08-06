612b928cd1c334033c503e396c6ef2e0b609597d
DONGMO BERNARD GERAUD <keenndjc@gmail.com>
2021-08-06 01:23:30 +0100

    Add campanies component to main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { IntroSectionComponent } from './components/intro-section/intro-section.component';
+import { CampaniesComponent } from './components/campanies/campanies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    IntroSectionComponent
+    IntroSectionComponent,
+    CampaniesComponent
   ],
   imports: [
     BrowserModule,
