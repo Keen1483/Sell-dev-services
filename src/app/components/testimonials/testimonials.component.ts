7faf6103de2bba1cabab425db50d67c055954a6b
DONGMO BERNARD GERAUD <keenndjc@gmail.com>
2021-08-13 10:16:11 +0100

    Create testimonial's model

--- src/app/components/testimonials/testimonials.component.ts
+++ src/app/components/testimonials/testimonials.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { TestimonialService } from 'src/app/services/testimonial.service';
+import { Testimonial } from 'src/app/models/Testimonial.model';
+import { Subscription } from 'rxjs';
+
+declare var $: any;
 
 @Component({
-  selector: 'app-testimonials',
-  templateUrl: './testimonials.component.html',
-  styleUrls: ['./testimonials.component.scss']
+    selector: 'app-testimonials',
+    templateUrl: './testimonials.component.html',
+    styleUrls: ['./testimonials.component.scss']
 })
-export class TestimonialsComponent implements OnInit {
+export class TestimonialsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    testimonialSubscription$: Subscription;
 
-  ngOnInit(): void {
-  }
+    testimonials: Testimonial[];
+
+    constructor(private testimonialService: TestimonialService) { }
+
+    ngOnInit(): void {
+        this.testimonialSubscription$ = this.testimonialService.testimonialSubject$.subscribe(
+            (testimonials: Testimonial[]) => {
+                this.testimonials = testimonials;
+            },
+            (error) => {
+                console.log('An error occurred : ' + error);
+            }
+        );
+        this.testimonialService.emitTestimonialSubject();
+
+        $(document).ready(() => {
+            if ($('.carousel-inner')) {
+                let carousel = document.querySelector('.carousel-inner');
+                $(carousel?.firstElementChild).addClass('active');
+            }
+        });
+    }
+
+    ngOnDestroy() {
+        this.testimonialSubscription$.unsubscribe();
+    }
 
 }
--- src/app/models/Testimonial.model.ts
+++ src/app/models/Testimonial.model.ts
@@ -1,4 +1,5 @@
 export interface Testimonial {
+    id: number
     thought: string,
     stars: number,
     name: string,
