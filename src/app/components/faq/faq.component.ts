84aa614da2f6823cdffd893c6c9a40ccc07ae659
DONGMO BERNARD GERAUD <keenndjc@gmail.com>
2021-08-14 21:09:29 +0100

    Coding formArray FAQ

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+
+declare var $: any;
 
 @Component({
     selector: 'app-faq',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqComponent implements OnInit {
 
-    constructor() { }
+    faqForm: FormGroup;
+
+    constructor(private fb: FormBuilder) { }
 
     ngOnInit(): void {
+        $(document).ready(() => {
+            $('.question').hide();
+
+            if (parseInt($('body').css('width')) <= 768) {
+                $('.ask-question').show();
+            }
+
+            $('.ask-question').click(function() {
+                $(".question").fadeOut("slow",function(){
+                    $(".question").fadeIn("slow");
+                });
+            });
+        });
+
+        this.initForm();
+    }
+
+    initForm() {
+        this.faqForm = this.fb.group({
+            email: ['', [Validators.required, Validators.email, Validators.maxLength(32)]],
+            questions: this.fb.array([this.questForm()])
+        });
+    }
+
+    questForm() {
+        return this.fb.group({
+            question: ['', [Validators.required, Validators.minLength(4)]]
+        });
+    }
+
+    getQuests() {
+        return this.faqForm.controls.questions as FormArray;
+    }
+
+    onAddQuests() {
+        this.getQuests().push(this.questForm());
+    }
+
+    onDeleteQuests(index: number) {
+        this.getQuests().removeAt(index);
+    }
+
+    onSubmit() {
+        const email = this.faqForm.get('email')?.value;
+        const questions = this.faqForm.get('questions')?.value;
+        console.log(email, questions);
     }
 
 }
---
+++ src/app/models/Portfolio.model.ts
@@ -0,0 +1,5 @@
+export interface Portfolio {
+    id: number,
+    project: string,
+    category: string
+}
