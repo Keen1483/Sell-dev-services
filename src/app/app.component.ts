0cb7c6417410ea91344f46ced8a034a8a6c2a354
DONGMO BERNARD GERAUD <keenndjc@gmail.com>
2021-08-17 09:39:39 +0100

    Configure project with Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+/* FIREBASE IMPORTS */
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
 declare var $: any;
 
 @Component({
@@ -9,6 +16,21 @@ declare var $: any;
 })
 export class AppComponent implements OnInit {
     title = 'app-avenue';
+
+    constructor() {
+        var firebaseConfig = {
+            apiKey: "AIzaSyBpyMRrcYnvUd0jf37tEMgEdv41QynaQZI",
+            authDomain: "appavenue-39fa3.firebaseapp.com",
+            projectId: "appavenue-39fa3",
+            storageBucket: "appavenue-39fa3.appspot.com",
+            messagingSenderId: "1088043996965",
+            appId: "1:1088043996965:web:277b0338d999dd0c222ccc",
+            measurementId: "G-0TEL55B908"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        firebase.analytics();
+    }
 
     ngOnInit() {
         $(document).ready(() => {
